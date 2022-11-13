4e2a30fe7936d192107015e1a7b570076508ba36
abel-haj <abel-haj@student.1337.ma>
2022-11-13 23:38:51 +0100

    images list page init

---
+++ pages/images/index.js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+
+export default function SeeAll() {
+  return (
+    <div className="container-fluid">
+      <Head>
+        <title>All Images - Cires Challenge</title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+
+      <main>
+      </main>
+    </div>
+  )
+}
