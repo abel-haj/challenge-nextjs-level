dd0a0ba659741ec6d0e74b215c872f0691683e1d
abel-haj <abel-haj@student.1337.ma>
2022-11-14 01:52:24 +0100

    charset meta added

--- pages/index.js
+++ pages/index.js
@@ -15,6 +15,7 @@ export default function Login() {
   return (
     <div className="container-fluid">
       <Head>
+        <meta charSet="UTF-8" />
         <title>Login - Cires Challenge</title>
         <link rel="icon" href="/favicon.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
