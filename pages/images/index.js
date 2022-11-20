e6d76e08b952e92fee2c6b3b456d52f870ac216d
Abdelaziz <techproduct@hotmail.com>
2022-11-20 06:53:39 +0100

    formatting page with vscode and getting liked images from database instead of a locally saving them

--- pages/images/index.js
+++ pages/images/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/Image'
+import Router from 'next/router'
 import { useState, useEffect } from 'react'
 import InfiniteScroll from 'react-infinite-scroll-component'
 import Header from '../../components/header'
@@ -100,68 +101,68 @@ export default function SeeAll() {
       <Header />
 
       <main className={``}>
-          <InfiniteScroll
-            className={`row mx-2 gap-4 justify-content-center image-container`}
-            prefill={true}
-            dataLength={images.length}
-            next={fetchImages}
-            hasMore={hasMore}
-            loader={
-              <div className="my-4 text-center">
-                <div className="spinner-border" role="status">
-                  <span className="visually-hidden">Loading...</span>
-                </div>
+        <InfiniteScroll
+          className={`row mx-2 gap-4 justify-content-center image-container`}
+          prefill={true}
+          dataLength={images.length}
+          next={fetchImages}
+          hasMore={hasMore}
+          loader={
+            <div className="my-4 text-center">
+              <div className="spinner-border" role="status">
+                <span className="visually-hidden">Loading...</span>
               </div>
-            }
-            endMessage={
-              <div className={`my-4 text-center`}>
-                <h3>Whoops! Looks like there are no more images to see!</h3>
-              </div>
-            }
-          >
-            {Object.keys(images).map((index) => {
-              return (
-                <div className={`position-relative rounded ${styles.image}`}
-                  key={index}>
+            </div>
+          }
+          endMessage={
+            <div className={`my-4 text-center`}>
+              <h3>Whoops! Looks like there are no more images to see!</h3>
+            </div>
+          }
+        >
+          {Object.keys(images).map((index) => {
+            return (
+              <div className={`position-relative rounded ${styles.image}`}
+                key={index}>
 
-                  <Image src={images[index].urls.regular} fill objectFit='cover' 
-                    className='rounded' sizes='25rem'
-                    alt={images[index].alt_description || index} 
-                  />
+                <Image src={images[index].urls.regular} fill objectFit='cover'
+                  className='rounded' sizes='25rem'
+                  alt={images[index].alt_description || index}
+                />
 
-                  <div className={`position-absolute bottom-0 start-0 end-0 p-2 bg-light rounded-bottom`}>
-                    <div className={`d-flex align-items-center justify-content-between`}>
-                      <div className={`text-nowrap ${styles.textEllipsis}`}>
-                        <Image
-                          className='rounded-circle'
-                          src={images[index].user.profile_image.small}
-                          width={24} height={24} />
-                        {` `}
-                        <b>{images[index].user.name}</b>
-                      </div>
-
-                      <a
-                        onClick={() => {
-                          // add image to likedImages or remove it
-                          if (likedImages.includes(images[index].id))
-                            setLikedImages(likedImages.filter((image) => image !== images[index].id))
-                          else
-                            setLikedImages([...likedImages, images[index].id])
-                        }}
-                        className={`text-nowrap ${likedImages.includes(images[index].id) ? 'text-danger' : 'text-muted'} ${styles.clickable} ${styles.zoomHover}`}
-                        >
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class={`bi bi-suit-heart-fill `} viewBox="0 0 16 16">
-                          <path d="M4 1c2.21 0 4 1.755 4 3.92C8 2.755 9.79 1 12 1s4 1.755 4 3.92c0 3.263-3.234 4.414-7.608 9.608a.513.513 0 0 1-.784 0C3.234 9.334 0 8.183 0 4.92 0 2.755 1.79 1 4 1z"/>
-                        </svg>
-                        {` `}
-                        <b>{ likedImages.includes(images[index].id) ? images[index].likes+1 : images[index].likes}</b>
-                      </a>
+                <div className={`position-absolute bottom-0 start-0 end-0 p-2 bg-light rounded-bottom`}>
+                  <div className={`d-flex align-items-center justify-content-between`}>
+                    <div className={`text-nowrap ${styles.textEllipsis}`}>
+                      <Image
+                        className='rounded-circle'
+                        src={images[index].user.profile_image.small}
+                        width={24} height={24} />
+                      {` `}
+                      <b>{images[index].user.name}</b>
                     </div>
+
+                    <a
+                      onClick={() => {
+                        // add image to likedImages or remove it
+                        if (likedImages.includes(images[index].id))
+                          handleUnlike(images[index].id)
+                        else
+                          handleLike(images[index].id)
+                      }}
+                      className={`text-nowrap ${likedImages.includes(images[index].id) ? 'text-danger' : 'text-muted'} ${styles.clickable} ${styles.zoomHover}`}
+                    >
+                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className={`bi bi-suit-heart-fill `} viewBox="0 0 16 16">
+                        <path d="M4 1c2.21 0 4 1.755 4 3.92C8 2.755 9.79 1 12 1s4 1.755 4 3.92c0 3.263-3.234 4.414-7.608 9.608a.513.513 0 0 1-.784 0C3.234 9.334 0 8.183 0 4.92 0 2.755 1.79 1 4 1z" />
+                      </svg>
+                      {` `}
+                      <b>{likedImages.includes(images[index].id) ? images[index].likes + 1 : images[index].likes}</b>
+                    </a>
                   </div>
                 </div>
-              )
-            })}
-          </InfiniteScroll>
+              </div>
+            )
+          })}
+        </InfiniteScroll>
       </main>
     </div>
   )
